3fcde2e73d75ffa0a0adfc0b41a91183f1ed5d5e
Samuel Dennis <sdhasjim@gmail.com>
2022-11-10 22:52:01 +0700

    Initial Commit

---
+++ LBTAChatAppFirebase/LBTAChatAppFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  LBTAChatAppFirebaseApp.swift
+//  LBTAChatAppFirebase
+//
+//  Created by Samuel Dennis on 01/11/22.
+//
+
+import SwiftUI
+
+@main
+struct LBTAChatAppFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
