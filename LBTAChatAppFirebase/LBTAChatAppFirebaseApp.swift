61167711e140fd9eade2a2c1dfe86a4159cb8062
Samuel Dennis <sdhasjim@gmail.com>
2022-12-04 20:05:51 +0700

    fetching current user uid, refactor firebase manager

--- LBTAChatAppFirebase/LBTAChatAppFirebaseApp.swift
+++ LBTAChatAppFirebase/LBTAChatAppFirebaseApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct LBTAChatAppFirebaseApp: App {
     var body: some Scene {
         WindowGroup {
-            LoginView()
+            MainMessagesView()
         }
     }
 }
--- LBTAChatAppFirebase/MainMessages/MainMessagesView.swift
+++ LBTAChatAppFirebase/MainMessages/MainMessagesView.swift
@@ -7,9 +7,75 @@
 
 import SwiftUI
 
+struct ChatUser {
+    let uid, email, profileImageUrl: String
+}
+
+class MainMessagesViewModel: ObservableObject {
+    
+    @Published var errorMessage = ""
+    @Published var chatUser: ChatUser?
+    
+    init() {
+        fetchCurrentUser()
+    }
+    
+    private func fetchCurrentUser() {
+        
+        guard let uid = FirebaseManager.shared.auth.currentUser?.uid else {
+            self.errorMessage = "Could not find firebase uid"
+            return
+        }
+        
+        FirebaseManager.shared.firestore.collection("users").document(uid).getDocument { snapshot, error in
+            if let error = error {
+                self.errorMessage = "Failed to fetch current user: \(error)"
+                print("Failed to fetch current user: ", error)
+                return
+            }
+            
+//            self.errorMessage = "123"
+            
+            guard let data = snapshot?.data() else {
+                self.errorMessage = "No data found"
+                return
+            }
+            
+//            self.errorMessage = "Data: \(data.description)"
+            let uid = data["uid"] as? String ?? ""
+            let email = data["email"] as? String ?? ""
+            let profileImageUrl = data["profileImageUrl"] as? String ?? ""
+            self.chatUser = ChatUser(uid: uid, email: email, profileImageUrl: profileImageUrl)
+            
+//            self.errorMessage = chatUser.profileImageUrl
+        }
+    }
+    
+}
+
 struct MainMessagesView: View {
     
     @State var shouldShowLogOutOptions = false
+    
+    @ObservedObject private var vm = MainMessagesViewModel()
+    
+    var body: some View {
+        NavigationView {
+
+            VStack {
+//                Text("USER: \(vm.chatUser?.uid ?? "")")
+                customNavBar
+                messagesView
+
+            }
+            .overlay(
+                newMessageButton, alignment: .bottom)
+            .navigationBarHidden(true)
+            
+
+//            .navigationTitle("Main Messages View")
+        }
+    }
     
     private var customNavBar: some View {
         HStack(spacing: 16) {
@@ -18,7 +84,7 @@ struct MainMessagesView: View {
                 .font(.system(size: 34, weight: .heavy))
             
             VStack(alignment: .leading, spacing: 4) {
-                Text("USERNAME")
+                Text("\(vm.chatUser?.email ?? "")")
                     .font(.system(size: 24, weight:.bold))
                 HStack {
                     Circle()
@@ -51,23 +117,7 @@ struct MainMessagesView: View {
             ])
         }
     }
-    
-    var body: some View {
-        NavigationView {
 
-            VStack {
-                customNavBar
-                messagesView
-
-            }
-            .overlay(
-                newMessageButton, alignment: .bottom)
-            .navigationBarHidden(true)
-            
-
-//            .navigationTitle("Main Messages View")
-        }
-    }
     
     private var messagesView: some View {
         ScrollView {
